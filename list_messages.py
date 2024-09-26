c8a5b22b14f74b2f974de5c286971f2ff8e0f3ba
Banana-89 <157877712+Banana-89@users.noreply.github.com>
2024-09-26 22:27:04 +0700

    Update list_messages.py

--- list_messages.py
+++ list_messages.py
@@ -1,24 +1,26 @@
 import socket
-import time
 
 PORT = 5050
 SERVER = "localhost"
 ADDR = (SERVER, PORT)
 FORMAT = "utf-8"
-DISCONNECT_MESSAGE = "!DISCONNECT"
-
 
 def connect():
+    """ Establish a connection to the server """
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(ADDR)
     return client
 
-
 def start():
+    """ Start listening for incoming messages """
     connection = connect()
     while True:
-        msg = connection.recv(1024).decode(FORMAT)
-        print(msg)
-
+        try:
+            msg = connection.recv(1024).decode(FORMAT)
+            if msg:
+                print(msg)
+        except:
+            print("Connection closed.")
+            break
 
 start()
